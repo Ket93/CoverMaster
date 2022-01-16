04ba0ee8a5d4247df1bbf8b89420918297737d5d
Andrey Karmanov <Andrey@karmanov.ca>
2022-01-15 19:49:12 -0500

    more updated

--- extension/background.js
+++ extension/background.js
@@ -12,29 +12,19 @@ console.log("Running")
 //   console.log('Value is: ', result.teststorage)
 // });
 
-//when a message is sent from another script, it goes through this listener
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.message === 'test_url') {
-//     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-//       const url = "http://http://34.130.223.251:25565/submit"
-//       var companyName = document.getElementsByClassName("table table-bordered")[3].rows[0].cells[1];
-//       var companyDivision = document.getElementsByClassName("table table-bordered")[3].rows[1].cells[1];
-//       var jobAddress = document.getElementsByClassName("np-view-question--18")[0];
-//       var jobCity = document.getElementsByClassName("np-view-question--20")[0];
-//       var jobProvince = document.getElementsByClassName("np-view-question--21")[0];
-//       var jobCountry = document.getElementsByClassName("np-view-question--23")[0];
-//       var jobPostal = document.getElementsByClassName("np-view-question--22")[0];
-
-//       //const url = "http://localhost:25565/submit"
-//       fetch(url, {
-//         method: "POST",
-//         body: JSON.stringify({ url: tabs[0]['url'] })
-//       }).then(response => response.text()).then(coverletterSITE => {
-//         //open coverlettersite
-//       });
-//     });
-//   };
-//   return true;
-// });
+// when a message is sent from another script, it goes through this listener
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'test_url') {
+        const url = "https://34.130.223.251:25565/submit"
+        console.log('sent something')
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(request.data)
+        }).then(response => response.text()).then(coverletterSITE => {
+            //open coverlettersite
+        });
+    };
+    return true;
+});
 
 
