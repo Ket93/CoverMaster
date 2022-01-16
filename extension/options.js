6ab308cbc6367ac168ece386c4b0d89b7e7e07b6
Andrey Karmanov <Andrey@karmanov.ca>
2022-01-16 10:08:23 -0500

    options fixed

--- extension/options.js
+++ extension/options.js
@@ -4,6 +4,8 @@ var nname = document.getElementById('name');
 var phone = document.getElementById('phone');
 var zip = document.getElementById('zip');
 var address = document.getElementById('address');
+var template = document.getElementById('file');
+var button = document.getElementById('save');
 var newOptions = null
 chrome.storage.local.get(['options'], function (result) {
     console.log('old options: ', result.options);
@@ -14,7 +16,8 @@ chrome.storage.local.get(['options'], function (result) {
             name: '',
             phone: '',
             address: '',
-            zip: ''
+            zip: '',
+            template: ''
         };
     };
     document.getElementById('email').value = newOptions['email'];
@@ -23,13 +26,37 @@ chrome.storage.local.get(['options'], function (result) {
     document.getElementById('zip').value = newOptions['zip'];
     document.getElementById('address').value = newOptions['address'];
 });
+template.onchange = function (event) {
+    var fileList = template.files;
+    console.log();
+    // chrome.storage.local.set({
+    //     options: { ...newOptions, template: fileList }
+    // }, () => console.log(newOptions));
+}
+const zero = 0n
+const shift = 8n
 
-document.getElementById('save').addEventListener('click', function () {
+function asciiToBinary(str) {
+    const len = str.length
+    let n = zero
+    for (let i = 0; i < len; i++) {
+        n = (n << shift) + BigInt(str.charCodeAt(i))
+    }
+    return n.toString(2).padStart(len * 8, 0)
+}
+button.addEventListener('click', async function () {
     newOptions['email'] = email.value;
     newOptions['name'] = nname.value;
     newOptions['phone'] = phone.value;
     newOptions['address'] = address.value;
     newOptions['zip'] = zip.value;
+    try {
+        newOptions['template'] = asciiToBinary(await template.files[0].text());
+    } catch (TypeError) {
+        console.log('no template')
+    }
+
+    console.log(newOptions['template'])
     chrome.storage.local.set({
         options: { ...newOptions }
     }, () => console.log(newOptions));
