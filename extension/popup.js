52aac1404c8ca2a1d090a96958cc2b769cce8b10
jodo3 <johndong3@homtail.com>
2022-01-15 21:00:28 -0500

    Added button to download which currently just goes to localhost:25565

--- extension/popup.js
+++ extension/popup.js
@@ -1,29 +1,5 @@
-// //js for the popup
-// let resultDiv = document.getElementById('result');
-// let background = document.getElementById('body');
-
-// function startUp() {
-//     resultDiv.innerHTML = 'Analyzing page...'
-//     chrome.runtime.sendMessage({
-//         message: 'test_url'
-//     }, response => updateBackground(response.html))
-// };
-
-// function updateBackground(info) {
-//     resultDiv.innerHTML = info
-//     if (info == 'Reliable') {
-//         background.classList.add('success')
-//     } else {
-//         background.classList.add('bad')
-//     };
-
-//     // chrome.storage.local.set({
-//     //     teststorage: info
-//     // });
-//     //sends message to the 'onMessage' listener in background.js
-//     // chrome.runtime.sendMessage({
-//     //     message: 'storage updated'
-//     // }, html => updateBackground(html));
-// }
-
-// startUp();
+document.querySelectorAll('INPUT[Type="BUTTON"]').forEach(button =>
+  button.addEventListener('click', event => {
+    window.location.href = event.target.dataset.location
+  })
+)
