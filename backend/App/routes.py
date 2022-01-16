c18d4b01343f1f205f5bb373d0ff329f72db037e
Kevin Cui <Kevincui113@gmail.com>
2022-01-15 21:07:12 -0500

    wrote company info to template file

--- backend/App/routes.py
+++ backend/App/routes.py
@@ -1,14 +1,20 @@
-from App import app
+from asyncore import write
+from App import app, evaluate
 from flask.globals import request
+
 
 @app.route('/')
 def index():
     return "invalid"
+
 
 @app.route('/submit', methods=['POST'])
 def sorting():
     if (request.get_json(force=True)):
         givenUrl = request.get_json(force=True)
         print(givenUrl)
+
+        evaluate.writeDoc(givenUrl)
+
         return "recieved data"
     return "did not recieve data"
--- extension/coverletter.js
+++ extension/coverletter.js
@@ -1,14 +1,14 @@
 
 var posting = document.querySelector("body > main > div.orbisModuleHeader > div > div > div:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(2) > span").innerText
 console.log("Running cover letter script")
-if (posting === 'Approved') {
+if (true) {
     // const url = "https://34.130.223.251:25565/submit"
     const url = "http://localhost:25565/submit"
 
     var companyName = document.getElementsByClassName("table table-bordered")[3].rows[0].cells[1];
     var companyDivision = document.getElementsByClassName("table table-bordered")[3].rows[1].cells[1];
     var jobAddress = document.getElementsByClassName("np-view-question--18")[0];
-    // var jobCity = document.getElementsByClassName("np-view-question--20")[0];
+    var jobCity = document.getElementsByClassName("np-view-question--20")[0];
     var jobProvince = document.getElementsByClassName("np-view-question--21")[0];
     var jobCountry = document.getElementsByClassName("np-view-question--23")[0];
     var jobPostal = document.getElementsByClassName("np-view-question--22")[0];
@@ -16,7 +16,7 @@ if (posting === 'Approved') {
         companyName: companyName.innerText || companyName.textContent,
         companyDivision: companyDivision.innerText || companyDivision.textContent,
         jobAddress: jobAddress.innerText || jobAddress.textContent,
-        // jobCity: jobCity.innerText || jobCity.textContent,
+        jobCity: jobCity.innerText || jobCity.textContent,
         jobProvince: jobProvince.innerText || jobProvince.textContent,
         jobCountry: jobCountry.innerText || jobCountry.textContent,
         jobPostal: jobPostal.innerText || jobPostal.textContent
