4fab559a3550de2350dc45cbd50f5c695c163f56
Anthony Ho <anthony52ho@gmail.com>
2022-01-15 23:41:56 -0500

    Update customworddoc.py
    WWWWW

--- backend/App/customworddoc.py
+++ backend/App/customworddoc.py
@@ -4,7 +4,59 @@ from docx.text.paragraph import Paragraph
 from docx.table import _Cell, Table
 from docx.oxml.table import CT_Tbl
 from docx.oxml.text.paragraph import CT_P
+import re
 
+def paragraph_replace_text(paragraph, regex, replace_str):
+    """Return `paragraph` after replacing all matches for `regex` with `replace_str`.
+
+    `regex` is a compiled regular expression prepared with `re.compile(pattern)`
+    according to the Python library documentation for the `re` module.
+    """
+    # --- a paragraph may contain more than one match, loop until all are replaced ---
+    while True:
+        text = paragraph.text
+        match = regex.search(text)
+        if not match:
+            break
+
+        # --- when there's a match, we need to modify run.text for each run that
+        # --- contains any part of the match-string.
+        runs = iter(paragraph.runs)
+        start, end = match.start(), match.end()
+
+        # --- Skip over any leading runs that do not contain the match ---
+        for run in runs:
+            run_len = len(run.text)
+            if start < run_len:
+                break
+            start, end = start - run_len, end - run_len
+
+        # --- Match starts somewhere in the current run. Replace match-str prefix
+        # --- occurring in this run with entire replacement str.
+        run_text = run.text
+        run_len = len(run_text)
+        run.text = "%s%s%s" % (run_text[:start], replace_str, run_text[end:])
+        end -= run_len  # --- note this is run-len before replacement ---
+
+        # --- Remove any suffix of match word that occurs in following runs. Note that
+        # --- such a suffix will always begin at the first character of the run. Also
+        # --- note a suffix can span one or more entire following runs.
+        for run in runs:  # --- next and remaining runs, uses same iterator ---
+            if end <= 0:
+                break
+            run_text = run.text
+            run_len = len(run_text)
+            run.text = run_text[end:]
+            end -= run_len
+
+    # --- optionally get rid of any "spanned" runs that are now empty. This
+    # --- could potentially delete things like inline pictures, so use your judgement.
+    # for run in paragraph.runs:
+    #     if run.text == "":
+    #         r = run._r
+    #         r.getparent().remove(r)
+
+    return paragraph
 
 def iter_block_items(parent):
     """
@@ -28,37 +80,18 @@ def iter_block_items(parent):
         elif isinstance(child, CT_Tbl):
             yield Table(child, parent)
 
-#doc = docx.Document("nwhacks cover letter.docx")
+doc = docx.Document("nwhacks cover letter.docx")
+def findReplace(doc, replaced, replace_str):
+    regex = re.compile(replaced)
+    for block in iter_block_items(doc):
+        if isinstance(block,Table):
+            ## is a table?
+            pass
+        else:
+            ## is a paragraph?
+            paragraph_replace_text(block, regex, replace_str) 
 
+findReplace(doc, "{{Name}}", "Kevin")
 
-# for block in iter_block_items(doc):
-#     if isinstance(block, Table):
-#         # is a table?
-#         pass
-#     else:
-#         # is a paragraph?
-#         print(block.text)
-
-
-def find_curly(text, toBeReplaced, replace):
-    line = text.split()
-    print(line)
-    for word in range(len(line)):
-
-        noPuncWord = ""
-        for char in line[word]:
-            if ord(char) >= 65 and ord(char) <= 90:
-                noPuncWord += char
-
-            elif ord(char) >= 97 and ord(char) <= 122:
-                noPuncWord += char
-
-        if noPuncWord == toBeReplaced and (line[word][0] == "{" and (line[word][-1] == "}" or line[word][-2] == "}")):
-            line[word] = replace
-
-    line = " ".join(line)
-    return line
-
-
-print(find_curly(
-    "May {{Name}} not turn out to be {{adjective}}.", "Name", "Kevin"))
+output = doc
+output.save('OutputDoc.docx')
